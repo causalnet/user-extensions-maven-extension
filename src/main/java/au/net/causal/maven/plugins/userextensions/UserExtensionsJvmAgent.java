bb48f8778dffd48a05ee5eed4f7f2296d99f3a11
Peter Runge <prunge@causal.net.au>
2023-02-02 22:47:50 +1100

    Enhanced interpolation feature that allows settings.xml properties from profiles to be used in extensions.xml interpolation and allow 'disabled' version to disable an extension.

--- src/main/java/au/net/causal/maven/plugins/userextensions/UserExtensionsJvmAgent.java
+++ src/main/java/au/net/causal/maven/plugins/userextensions/UserExtensionsJvmAgent.java
@@ -5,6 +5,7 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.LoaderClassPath;
+import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
@@ -31,9 +32,9 @@ public class UserExtensionsJvmAgent
             public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer)
             throws IllegalClassFormatException
             {
-                if ("org/apache/maven/cli/MavenCli".equals(className))
+                try
                 {
-                    try
+                    if ("org/apache/maven/cli/MavenCli".equals(className))
                     {
                         ClassPool classPool = new ClassPool(null);
                         classPool.appendClassPath(new LoaderClassPath(loader));
@@ -46,10 +47,26 @@ public class UserExtensionsJvmAgent
                         classfileBuffer = ctClass.toBytecode();
                         ctClass.detach();
                     }
-                    catch (Exception e)
+                    else if ("org/apache/maven/cli/internal/BootstrapCoreExtensionManager".equals(className))
                     {
-                        e.printStackTrace();
+                        ClassPool classPool = new ClassPool(null);
+                        classPool.appendClassPath(new LoaderClassPath(loader));
+
+                        CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+
+                        CtMethod resolveCoreExtensionsMethod = ctClass.getDeclaredMethod("resolveCoreExtensions");
+                        transformResolveCoreExtensionsMethod(resolveCoreExtensionsMethod);
+
+                        CtMethod createInterpolatorMethod = ctClass.getDeclaredMethod("createInterpolator");
+                        transformCreateInterpolatorMethod(createInterpolatorMethod);
+
+                        classfileBuffer = ctClass.toBytecode();
+                        ctClass.detach();
                     }
+                }
+                catch (Throwable e)
+                {
+                    e.printStackTrace();
                 }
                 return classfileBuffer;
             }
@@ -98,6 +115,8 @@ public class UserExtensionsJvmAgent
 
 
                             //Now try a user-global extensions.xml file, if it exists
+                            //"System.err.println(\"userProperties: \" + cliRequest.userProperties);" +
+                            //TODO support reading the user extensions.xml location from properties
                             "if (new java.io.File(USER_MAVEN_CONFIGURATION_HOME, \"extensions.xml\").isFile()) { $_.addAll($proceed(new java.io.File(USER_MAVEN_CONFIGURATION_HOME, \"extensions.xml\"))); } " +
 
                             //Dedupe the entries
@@ -106,6 +125,69 @@ public class UserExtensionsJvmAgent
                 }
             }
         });
+    }
+
+    private static void transformResolveCoreExtensionsMethod(CtMethod m)
+    throws CannotCompileException
+    {
+        m.instrument(new ExprEditor()
+        {
+            @Override
+            public void edit(MethodCall c) throws CannotCompileException
+            {
+                //If the version of the plugin after interpolation is determined to be 'disabled' then disregard that extension
+                if ("org.apache.maven.cli.internal.BootstrapCoreExtensionManager".equals(c.getClassName()) && "resolveExtension".equals(c.getMethodName()))
+                {
+                    c.replace(
+                        "System.err.println(\"*** \" + $5.interpolate($1.getVersion())); " +
+                        "if (\"disabled\".equalsIgnoreCase($5.interpolate($1.getVersion()))) { $_ = java.util.Collections.emptyList(); }" +
+                        "else { $_ = $proceed($$); }"
+                    );
+                }
+            }
+        });
+    }
+
+    private static void transformCreateInterpolatorMethod(CtMethod m)
+    throws CannotCompileException
+    {
+        try
+        {
+            m.addLocalVariable("i", m.getDeclaringClass().getClassPool().get("java.util.ListIterator"));
+            m.addLocalVariable("p", m.getDeclaringClass().getClassPool().get("org.apache.maven.model.Profile"));
+            m.addLocalVariable("p2", m.getDeclaringClass().getClassPool().get("java.util.Properties"));
+            m.insertAfter(
+                    "org.apache.maven.model.building.DefaultModelBuilder builder = new org.apache.maven.model.building.DefaultModelBuilderFactory().newInstance();" +
+
+                    //Don't care that this model building fails (using a directory as POM file) but we want to grab the active profiles from the exception at the end
+                    //and that gets resolved whether the POM file is real or not
+                    "org.apache.maven.model.building.DefaultModelBuildingRequest request = new org.apache.maven.model.building.DefaultModelBuildingRequest().setPomFile(new java.io.File(\".\"));" +
+                    "request.setProfiles($1.getProfiles());" +
+                    "request.setActiveProfileIds($1.getActiveProfiles());" +
+                    "request.setInactiveProfileIds($1.getInactiveProfiles());" +
+                    "request.setSystemProperties($1.getSystemProperties());" +
+                    "request.setUserProperties($1.getUserProperties());" +
+                    "org.apache.maven.model.building.ModelBuildingResult result = null;" +
+                    "try {" +
+                    "  result = builder.build(request);" +
+                    "} catch (org.apache.maven.model.building.ModelBuildingException ex) {" +
+                    "  result = ex.getResult();" +
+                    "} " +
+                    "java.util.List activeProfiles = result.getActiveExternalProfiles();" +
+
+                    //Now we have resolved active profiles, add value sources in reverse order
+                    //This is the way Maven works as well normally for properties in multiple profiles (later profiles should win), tested with Antrun
+                    "for (i = activeProfiles.listIterator(activeProfiles.size()); i.hasPrevious();) { " +
+                    "   p = i.previous();" +
+                    "   p2 = org.apache.maven.model.Profile.class.getMethod(\"getProperties\", null).invoke(p, null);" +
+                    "   $_.addValueSource(new org.codehaus.plexus.interpolation.MapBasedValueSource(p2));" +
+                    "}"
+            );
+        }
+        catch (NotFoundException e)
+        {
+            throw new CannotCompileException(e);
+        }
     }
 
     /**
@@ -119,6 +201,7 @@ public class UserExtensionsJvmAgent
      *
      * @param extensionEntries a list of {@link org.apache.maven.cli.internal.extension.model.CoreExtension} objects.
      */
+    //TODO don't use this we want to avoid having to make extra class available
     public static <T> void deduplicateExtensionEntries(List<T> extensionEntries)
     {
         Set<ExtensionEntry<T>> extensionEntriesSet = extensionEntries.stream()
